36ff5230f54bbaf2392443223bfaac6ac53fe7e1
Andrey Rouskol <arouskol@yandex.ru>
2022-09-13 18:34:05 +0300

    first module test and first fake url

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 # Create your tests here.
-class SmokeTest(TestCase):
-    '''тест на токсичность'''
+class HomePageTest(TestCase):
+    '''test for correct home page'''
 
-    def test_bad_maths(self):
-        '''тест: неправильные математические вычислениея'''
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_views(self):
+        '''тест: корневой url преобразуется в представление домашней страницы'''
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
