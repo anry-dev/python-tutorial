472e4f480d0224b578a27f8f56a471e8d18b6510
Andrey Rouskol <arouskol@yandex.ru>
2022-09-13 18:44:22 +0300

    first html home page

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -10,3 +11,14 @@ class HomePageTest(TestCase):
         '''тест: корневой url преобразуется в представление домашней страницы'''
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        '''test: home page must return correct html'''
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
