19e967955a710f30e18e61ee31d30131413ccab8
Andrey Rouskol <arouskol@yandex.ru>
2022-09-14 12:21:59 +0300

    tests done using templates

--- lists/tests.py
+++ lists/tests.py
@@ -13,26 +13,6 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
-        '''test: home page must return correct html'''
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-
-    def test_home_page_uses_correct_template(self):
-        '''test: home page must return correct html'''
-
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        expected_html = render_to_string('home.html')
-        self.assertEqual(html, expected_html)
-
     def test_root_url_returns_correct_html(self):
         '''test: home page must return correct html'''
 
@@ -43,4 +23,4 @@ class HomePageTest(TestCase):
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
 
-        self.assertTemplateUsed(response, 'wrong-home.html')
+        self.assertTemplateUsed(response, 'home.html')
