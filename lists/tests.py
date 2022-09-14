880ab88e8bda2012b301966b5eb29254ab7d265f
Andrey Rouskol <arouskol@yandex.ru>
2022-09-14 18:42:41 +0300

    redirect after POST and saving user input

--- lists/tests.py
+++ lists/tests.py
@@ -15,29 +15,48 @@ class HomePageTest(TestCase):
 
     def test_root_url_returns_correct_html(self):
         '''test: home page must return correct html'''
-
         response = self.client.get('/')
-
         html = response.content.decode('utf8')
         self.assertTemplateUsed(response, 'home.html')
 
     def test_can_save_a_POST_request(self):
         '''test: home pages save a POST request'''
-
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_get_redirect_after_POST_request(self):
+        '''test: POST request return redirect to home page'''
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+        ##self.assertIn('A new list item', response.content.decode())
+        ##self.assertTemplateUsed(response, 'home.html')
+
+    def test_save_items_only_when_necessary(self):
+        '''test: save items only when it is needed'''
+        response = self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        '''test: home page displays all list elements'''
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('item 1', response.content.decode())
+        self.assertIn('item 2', response.content.decode())
 
 class ItemModelTest(TestCase):
     '''List elements model tests'''
 
     def test_saving_and_retrieving_items(self):
         '''test to save and retrieve an item'''
-
         first_item = Item()
         first_item.text = 'Just the first item!'
         first_item.save()
-
         second_item = Item()
         second_item.text = 'The second item.'
         second_item.save()
@@ -49,4 +68,3 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'Just the first item!')
         self.assertEqual(second_saved_item.text, 'The second item.')
-
