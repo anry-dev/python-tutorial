65ccf4322408b1562f5b5a005868c9ec3a8002cc
Andrey Rouskol <arouskol@yandex.ru>
2022-09-14 12:20:56 +0300

    start using templates, transforming tests

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -21,4 +22,25 @@ class HomePageTest(TestCase):
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
+
+    def test_home_page_uses_correct_template(self):
+        '''test: home page must return correct html'''
+
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        expected_html = render_to_string('home.html')
+        self.assertEqual(html, expected_html)
+
+    def test_root_url_returns_correct_html(self):
+        '''test: home page must return correct html'''
+
+        response = self.client.get('/')
+
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'wrong-home.html')
