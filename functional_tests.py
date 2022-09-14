965718174d59a4628195dca369dd0c7aaa70751f
Andrey Rouskol <arouskol@yandex.ru>
2022-09-14 14:49:08 +0300

    start working with POST - not finished

--- functional_tests.py
+++ functional_tests.py
@@ -48,21 +48,30 @@ class NewVisitonTest(unittest.TestCase):
 
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertTrue(
-                any(row.text == '1: Купить павлиньи перья' for row in rows),
-                "Новый элемент списка не появился в таблице"
-        )
+        #self.assertTrue(
+        #        any(row.text == '1: Купить павлиньи перья' for row in rows),
+        #        f"Новый элемент списка не появился в таблице. Содержимое было:\n'{table.text}'"
+        #)
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
         # (Эдит очень методична)
-        self.fail('Закончить написание тестов!!!')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # Страница снова обновляется, и теперь показывает оба элемента ее списка
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+        self.assertIn('2: Сделать мушку из павлиньих перьев', [row.text for row in rows])
 
 
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
         # сайт сгенерировал для нее уникальный URL-адрес – об этом
+        self.fail('Закончить написание тестов!!!')
 
         # выводится небольшой текст с объяснениями.
         # Она посещает этот URL-адрес – ее список по-прежнему там.
--- lists/views.py
+++ lists/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 
 def home_page(request):
     '''home page'''
-    return render(request, 'home.html')
+
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', '')
+        })
