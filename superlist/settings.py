5032cbcdf4516091bdfa3eeb68031d741c37a170
Andrey Rouskol <arouskol@yandex.ru>
2022-10-02 22:12:43 +0300

    database mooved outside project root

--- superlist/settings.py
+++ superlist/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'superlist.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': Path.resolve(PurePath.joinpath(BASE_DIR, '../database/db.sqlite3')),
     }
 }
 
