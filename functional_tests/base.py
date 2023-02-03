1ebf6062e78ea2a6b29d16b9b1c28f4238345d86
Andrey Rouskol <arouskol@yandex.ru>
2023-02-03 22:52:27 +0300

    decorators added

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -40,36 +40,56 @@ class FunctionalTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
+    def wait(fn):
+        '''wait decorator'''
+        def modified_fn(*args, **kwargs):
+            start_time = time.time()
+            while True:
+                try:
+                    return fn(*args, **kwargs)
+                except (AssertionError, WebDriverException) as e:
+                    if time.time() - start_time > MAX_WAIT:
+                        raise e
+                    time.sleep(0.5)
+        return modified_fn
 
+    def waitt(wait_time = MAX_WAIT, step = 0.5):
+        '''wait decorator with args
+           must be placed as:
+           @waitt(args)
+        '''
+        def decorator(fn):
+            def wrapper(*args, **kwargs):
+                start_time = time.time()
+                while True:
+                    try:
+                        return fn(*args, **kwargs)
+                    except (AssertionError, WebDriverException) as e:
+                        if time.time() - start_time > MAX_WAIT:
+                            raise e
+                        time.sleep(step)
+            return wrapper 
+        return decorator
+
+
+    @wait
     def wait_for(self, fn):
-        '''wait for fn to become True'''
-        start_time = time.time()
-        while True:
-            try:
-                return fn()
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+        return fn()
 
     def get_item_input_box(self):
         '''return input field for the form'''
         return self.browser.find_element(By.ID, 'id_text')
 
+    @wait
     def wait_to_be_logged_in(self, email):
         '''waiting for user with email to be logged in'''
-        self.wait_for(
-                lambda: self.browser.find_element(By.LINK_TEXT, 'Log out')
-        )
-
+        self.browser.find_element(By.LINK_TEXT, 'Log out')
         navbar = self.browser.find_element(By.CSS_SELECTOR, '.navbar')
         self.assertIn(email, navbar.text)
 
+    @wait
     def wait_to_be_logged_out(self, email):
         '''waiting for user to log out'''
-        self.wait_for(
-                lambda: self.browser.find_element(By.NAME, 'email')
-        )
-
+        self.browser.find_element(By.NAME, 'email')
         navbar = self.browser.find_element(By.CSS_SELECTOR, '.navbar')
         self.assertNotIn(email, navbar.text)
