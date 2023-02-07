8bd3b651f8a37d42c05a7c7012fcfa94d688ab24
Andrey Rouskol <arouskol@yandex.ru>
2023-02-07 10:34:27 +0300

    Fix 'Session data corrupt' error when running on remote host

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -3,11 +3,12 @@ from fabric.context_managers import settings
 
 import logging
 logger = logging.getLogger(__name__)
-logger.debug(f'module: {__name__}')
 
 def _get_manage_dot_py(host):
     '''return command line to run manage.py for the site'''
-    return f'~/sites/{host}/virtualenv/bin/python ~/sites/{host}/source/manage.py'
+    # doesn't work on remote host with sessions - Sesstion data corrupt
+    #return f'~/sites/{host}/virtualenv/bin/python ~/sites/{host}/source/manage.py'
+    return f'cd ~/sites/{host}/source && ~/sites/{host}/virtualenv/bin/python ~/sites/{host}/source/manage.py'
 
 def reset_database(host):
     '''reset database on remote host'''
@@ -20,5 +21,11 @@ def create_session_on_server(host, email):
     manage_dot_py = _get_manage_dot_py(host)
     with settings(host_string=f'{host}'):
         session_key = run(f'{manage_dot_py} create_session {email}')
-        logger.debug(f'session_key.strip: "{session_key.strip()}"')
         return session_key.strip()
+
+def create_token_on_server(host, email):
+    '''create auth token on the remote host'''
+    manage_dot_py = _get_manage_dot_py(host)
+    with settings(host_string=f'{host}'):
+        token = run(f'{manage_dot_py} create_token {email}')
+        return token.strip()
