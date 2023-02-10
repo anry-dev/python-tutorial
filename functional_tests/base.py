68b4413a33921069e262309c53309235684e8a36
Andrey Rouskol <arouskol@yandex.ru>
2023-02-10 11:21:30 +0300

    error block fixed

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -10,15 +10,59 @@ import unittest
 import os
 from .server_tools import reset_database
 
+import logging
+logger = logging.getLogger(__name__)
+
 # screen dumps config
 from datetime import datetime
 from pathlib import Path
+
+# create_pre_auth_session
+from django.conf import settings
+from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
+from .server_tools import create_session_on_server, create_token_on_server
+from .management.commands.create_session import create_pre_authenticated_session
+from .management.commands.create_token import create_auth_token
+User = get_user_model()
 
 # Path(__file__) - full path to base.py
 SCREEN_DUMP_LOCATION = Path(__file__).resolve().parent.joinpath('screendumps')
 #SCREEN_DUMP_LOCATION = Path(__file__).resolve().parent.parent.parent.joinpath('screendumps')
 
 MAX_WAIT = 10
+
+
+def wait(fn):
+        '''wait decorator'''
+        def modified_fn(*args, **kwargs):
+            start_time = time.time()
+            while True:
+                try:
+                    return fn(*args, **kwargs)
+                except (AssertionError, WebDriverException) as e:
+                    if time.time() - start_time > MAX_WAIT:
+                        raise e
+                    time.sleep(0.5)
+        return modified_fn
+
+def waitt(wait_time = MAX_WAIT, step = 0.5):
+        '''wait decorator with args
+           must be placed as:
+           @waitt(args)
+        '''
+        def decorator(fn):
+            def wrapper(*args, **kwargs):
+                start_time = time.time()
+                while True:
+                    try:
+                        return fn(*args, **kwargs)
+                    except (AssertionError, WebDriverException) as e:
+                        if time.time() - start_time > MAX_WAIT:
+                            raise e
+                        time.sleep(step)
+            return wrapper 
+        return decorator
+
 
 class FunctionalTest(StaticLiveServerTestCase):
     '''функциональный тест - обертка'''
@@ -76,51 +120,12 @@ class FunctionalTest(StaticLiveServerTestCase):
         with open(filename, 'w') as f:
             f.write(self.browser.page_source)
 
+    @wait
     def wait_for_row_in_list_table(self, row_text):
         '''wait for a row to present in the list table'''
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element(By.ID, 'id_list_table')
-                rows = table.find_elements(By.TAG_NAME, 'tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-
-    def wait(fn):
-        '''wait decorator'''
-        def modified_fn(*args, **kwargs):
-            start_time = time.time()
-            while True:
-                try:
-                    return fn(*args, **kwargs)
-                except (AssertionError, WebDriverException) as e:
-                    if time.time() - start_time > MAX_WAIT:
-                        raise e
-                    time.sleep(0.5)
-        return modified_fn
-
-    def waitt(wait_time = MAX_WAIT, step = 0.5):
-        '''wait decorator with args
-           must be placed as:
-           @waitt(args)
-        '''
-        def decorator(fn):
-            def wrapper(*args, **kwargs):
-                start_time = time.time()
-                while True:
-                    try:
-                        return fn(*args, **kwargs)
-                    except (AssertionError, WebDriverException) as e:
-                        if time.time() - start_time > MAX_WAIT:
-                            raise e
-                        time.sleep(step)
-            return wrapper 
-        return decorator
-
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     @wait
     def wait_for(self, fn):
@@ -150,3 +155,35 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.browser.find_element(By.NAME, 'email')
         navbar = self.browser.find_element(By.CSS_SELECTOR, '.navbar')
         self.assertNotIn(email, navbar.text)
+
+
+    def create_pre_authenticated_session_by_token(self, email):
+        '''make web sessions authenticated on the server'''
+        if self.staging_server:
+            token = create_token_on_server(self.staging_server, email)
+            #self.browser.get(url)
+        else:
+            token = create_auth_token(email)
+
+        url = f'{self.live_server_url}/accounts/login?token={token}'
+        self.browser.get(url)
+
+    def create_pre_authenticated_session_by_session(self, email):
+        '''make web sessions authenticated on the server'''
+        if self.staging_server:
+            session_key = create_session_on_server(self.staging_server, email)
+        else:
+            session_key = create_pre_authenticated_session(email)
+
+        # set cookies for the browser
+        # page 404 loads faster then other
+        self.browser.get(self.live_server_url + '/404_no_such_url/')
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session_key,
+            path='/',
+        ))
+
+    def create_pre_authenticated_session(self, email):
+        '''create session shortcut'''
+        return self.create_pre_authenticated_session_by_session(email)
--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -1,46 +1,12 @@
-from django.conf import settings
-from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
 from django.contrib.sessions.backends.db import SessionStore
 from selenium.webdriver.common.by import By
 from .base import FunctionalTest
-from .server_tools import create_session_on_server, create_token_on_server
-from .management.commands.create_session import create_pre_authenticated_session
-from .management.commands.create_token import create_auth_token
 
 import logging
 logger = logging.getLogger(__name__)
 
-User = get_user_model()
-
 class MyListsTest(FunctionalTest):
     '''test users own todo lists'''
-
-    def create_pre_authenticated_session_by_token(self, email):
-        '''make web sessions authenticated on the server'''
-        if self.staging_server:
-            token = create_token_on_server(self.staging_server, email)
-            #self.browser.get(url)
-        else:
-            token = create_auth_token(email)
-
-        url = f'{self.live_server_url}/accounts/login?token={token}'
-        self.browser.get(url)
-
-    def create_pre_authenticated_session_by_session(self, email):
-        '''make web sessions authenticated on the server'''
-        if self.staging_server:
-            session_key = create_session_on_server(self.staging_server, email)
-        else:
-            session_key = create_pre_authenticated_session(email)
-
-        # set cookies for the browser
-        # page 404 loads faster then other
-        self.browser.get(self.live_server_url + '/404_no_such_url/')
-        self.browser.add_cookie(dict(
-            name=settings.SESSION_COOKIE_NAME,
-            value=session_key,
-            path='/',
-        ))
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
         '''test: lists for the authenticated users are saved as my_lists'''
